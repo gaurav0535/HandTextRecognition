eb44df82a056d180129c6c5b1435939df69548ed
gaurav0535 <gaurav0535@hotmail.com>
2020-04-18 17:48:49 +0530

    Library imported

--- src/SamplePreprocessor.py
+++ src/SamplePreprocessor.py
@@ -5,3 +5,40 @@ import random
 import numpy as np
 import cv2
 
+
+def preprocess(img=1, imgSize=2, dataAugmentation=False):
+    "put image into target img of size imgSize, transpose for TF and normalize gray-values"
+
+    # for damaged files in IAM data-set -use black image instead
+
+    if img is None:
+        img = np.zeros([imgSize[1], imgSize[0]])
+
+    # Apply data augmentation
+    if dataAugmentation:
+        stretch = (random.random() - .5)
+        wStretched = max(int(img.shape[1] * (1 + stretch)), 1)  # random width, but at least 1
+        img = cv2.resize(img, (wStretched, img.shape[0]))  # stretch horizontally by factor 0.5 .. 1.5
+
+    # create target image and copy sample image into it
+    (wt,ht) = imgSize
+    (h,w) = img.shape
+    fx = w/wt
+    fy = h/ht
+    f = max(fx,fy)
+    newSize = (max(min(wt, int(w / f)), 1),
+               max(min(ht, int(h / f)), 1))  # scale according to f (result at least 1 and at most wt or ht)
+    img = cv2.resize(img, newSize)
+    target = np.ones([ht, wt]) * 255
+    target[0:newSize[1], 0:newSize[0]] = img
+
+    # transpose for TF
+    img = cv2.transpose(target)
+
+    # normalize
+    (m, s) = cv2.meanStdDev(img)
+    m = m[0][0]
+    s = s[0][0]
+    img = img - m
+    img = img / s if s > 0 else img
+    return img
